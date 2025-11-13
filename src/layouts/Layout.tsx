047348a2c1ab70a905e67fa2bf7181295206a3cb
jinj00oo <icems0428@naver.com>
2025-11-13 14:26:28 +0900

    chore: 레이아웃 css 설정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,11 +3,15 @@ import BottomNavbar from '../components/organisms/BottomNavBar/BottomNavbar';
 
 const Layout = () => {
   return (
-    <div className="flex h-[48.75rem] w-[22.5rem] flex-col">
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <BottomNavbar />
+    <div className="flex justify-center bg-gray-100">
+      <div className="flex h-dvh w-dvw flex-col bg-white sm:w-[360px]">
+        <main className="flex flex-1">
+          <Outlet />
+        </main>
+        <footer>
+          <BottomNavbar />
+        </footer>
+      </div>
     </div>
   );
 };
