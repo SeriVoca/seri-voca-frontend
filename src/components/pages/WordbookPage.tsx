9c15831c30f525fd1f5644b38f3f492de07cab47
Emithen <86219540+Emithen@users.noreply.github.com>
2025-11-20 14:42:16 +0900

    fix: 머지 충돌 미해결 문제 해결

--- src/components/pages/WordbookPage.tsx
+++ src/components/pages/WordbookPage.tsx
@@ -21,12 +21,3 @@ const tabs = [
   { id: 1, label: '커리큘럼' },
   { id: 2, label: '내 단어장' },
 ];
-const WordbookPage = () => {
-  return (
-    <div>
-      <h1>Wordbook Page</h1>
-    </div>
-  );
-};
-
-export default WordbookPage;
