e5506d5719c790a8e101a94507ba336d4b3cfca6
jinj00oo <icems0428@naver.com>
2025-11-25 16:00:43 +0900

    feat: 단어장 상세 페이지에 단어 리스트 적용 #17

--- src/components/pages/WordbookDetailPage.tsx
+++ src/components/pages/WordbookDetailPage.tsx
@@ -1,17 +1,39 @@
 import { useNavigate } from 'react-router-dom';
 import Header from '../organisms/Header/Header';
+import type { WordDetailProps } from '../organisms/Word/WordDetail';
+import { WordList } from '../organisms/Word/WordList';
 
 export const WordbookDetailPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex h-full w-full flex-col">
       <Header
         title="단어장 상세 페이지"
         variant="LCTA"
         LCTAIcon="ChevronLeft"
         onLCTAClick={() => navigate(-1)}
       />
-      <div>WordbookDetailPage</div>
+      <div className="flex h-full w-full flex-1 flex-col overflow-y-auto">
+        <WordList words={mockWordList} />
+      </div>
     </div>
   );
 };
+
+export const mockWordList: WordDetailProps[] = [
+  { engWord: 'abstract', korWord: '추상적인' },
+  { engWord: 'benevolent', korWord: '자애로운' },
+  { engWord: 'contemplate', korWord: '숙고하다' },
+  { engWord: 'diligent', korWord: '성실한' },
+  { engWord: 'elevate', korWord: '향상시키다' },
+  { engWord: 'abstract', korWord: '추상적인' },
+  { engWord: 'benevolent', korWord: '자애로운' },
+  { engWord: 'contemplate', korWord: '숙고하다' },
+  { engWord: 'diligent', korWord: '성실한' },
+  { engWord: 'elevate', korWord: '향상시키다' },
+  { engWord: 'abstract', korWord: '추상적인' },
+  { engWord: 'benevolent', korWord: '자애로운' },
+  { engWord: 'contemplate', korWord: '숙고하다' },
+  { engWord: 'diligent', korWord: '성실한' },
+  { engWord: 'elevate', korWord: '향상시키다' },
+];
