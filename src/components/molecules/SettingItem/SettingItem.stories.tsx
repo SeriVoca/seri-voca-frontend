9fec3fbcdc58da606209246a2b54197d9762d410
jinj00oo <icems0428@naver.com>
2025-11-30 02:07:30 +0900

    feat: 설정 버튼 컴포넌트 제작

---
+++ src/components/molecules/SettingItem/SettingItem.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { SettingItem } from './SettingItem';
+
+const meta: Meta<typeof SettingItem> = {
+  title: 'Molecules/SettingItem',
+  component: SettingItem,
+  tags: ['autodocs'],
+  args: {
+    label: '로그아웃',
+    handleNavigate: () => alert('clicked!'),
+  },
+  argTypes: {
+    label: { control: 'text' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SettingItem>;
+
+export const Default: Story = {};
+
+export const Withdraw: Story = {
+  args: {
+    label: '회원탈퇴',
+  },
+};
