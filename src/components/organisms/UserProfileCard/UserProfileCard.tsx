f1975a479121af10181911e7e408c31ee61e283b
jinj00oo <157488644+jinj00oo@users.noreply.github.com>
2025-12-01 16:57:55 +0900

    [FEAT] 회원 정보 섹션 컴포넌트 제작 (#26)
    * feat: 유저 프로필 카드 컴포넌트 제작
    
    * chore: 스토리북 계층 수정
    
    * fix: padding 대칭
    
    ---------
    
    Co-authored-by: Emithen <86219540+Emithen@users.noreply.github.com>

---
+++ src/components/organisms/UserProfileCard/UserProfileCard.tsx
@@ -0,0 +1,25 @@
+type Props = {
+  profileImage: string | null;
+  name: string;
+  email: string;
+};
+
+export const UserProfileCard = ({ profileImage, name, email }: Props) => {
+  return (
+    <section className="flex px-[1.75rem] py-[1.25rem]">
+      <div className="flex items-center gap-[1.75rem]">
+        <div className="h-[4.5rem] w-[4.5rem] overflow-hidden rounded-full">
+          {profileImage ? (
+            <img src={profileImage} alt="프로필 이미지" className="h-full w-full object-cover" />
+          ) : (
+            <div className="h-full w-full bg-gray-200" />
+          )}
+        </div>
+        <div className="flex flex-col gap-[0.25rem]">
+          <h2 className="text-xl font-semibold">{name}님</h2>
+          <p className="text-gray-400">{email}</p>
+        </div>
+      </div>
+    </section>
+  );
+};
