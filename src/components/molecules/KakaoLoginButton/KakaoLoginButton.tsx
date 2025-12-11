f78f729118cc298c7ff47a0996ae20d1fa14e674
Emithen <86219540+Emithen@users.noreply.github.com>
2025-12-12 02:25:17 +0900

    feat: login test 성공

--- src/components/molecules/KakaoLoginButton/KakaoLoginButton.tsx
+++ src/components/molecules/KakaoLoginButton/KakaoLoginButton.tsx
@@ -6,7 +6,7 @@ const KakaoLoginButton = ({ handleClick }: Props) => {
   return (
     <button
       onClick={handleClick}
-      className="flex h-[3rem] w-full items-center rounded-[0.75rem] bg-[#FEE500] px-[1.5rem]"
+      className="flex h-[3rem] w-full cursor-pointer items-center rounded-[0.75rem] bg-[#FEE500] px-[1.5rem]"
     >
       <img src="src/assets/chat-bubble.svg" alt="카카오 로고" />
       <div className="flex-1 text-[15px] font-semibold">카카오 로그인</div>
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,15 +1,22 @@
+import supabase from '../../apis/supabaseInstance';
 import KakaoLoginButton from '../molecules/KakaoLoginButton/KakaoLoginButton';
 
 const LoginPage = () => {
-  const handleClick = () => {
-    // login
+  const signInWithKakao = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'kakao',
+      options: {
+        redirectTo: 'http://localhost:5173/wordbooks',
+      },
+    });
+    console.log(data, error);
   };
 
   return (
     <div className="flex h-full w-full flex-col items-center bg-white">
       <div className="mt-[12.5rem] text-[3rem]">Serivoca</div>
       <div className="mt-auto w-full p-5">
-        <KakaoLoginButton handleClick={handleClick} />
+        <KakaoLoginButton handleClick={signInWithKakao} />
       </div>
     </div>
   );
