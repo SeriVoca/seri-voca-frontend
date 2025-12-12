6e6c201446102c0a97c7e57a829d5b6fa17b790f
jinj00oo <icems0428@naver.com>
2025-12-12 23:38:01 +0900

    chore: 단어 api 응답 타입 정의

---
+++ src/api/word/types.ts
@@ -0,0 +1,11 @@
+export interface WordResponse {
+  id: string;
+  en_text: string;
+  order_index: number;
+  meanings: {
+    part_of_speech: string;
+    meaning: string;
+    order_index: number;
+    created_at: string;
+  }[];
+}
