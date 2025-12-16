f768fc441ba63b6592e7ba09009e30a219bc866e
jinj00oo <157488644+jinj00oo@users.noreply.github.com>
2025-12-16 15:01:27 +0900

    [FEAT] 단어 리스트 조회 api 연동 (#34)
    * chore: axios, supabase 라이브러리 설치
    
    * chroe: axiosInstance, supabaseInstance 설정
    
    * chore: .gitignore에 .env 추가
    
    * chore: 단어 api 응답 타입 정의
    
    * chore: 단어 도메인 및 매퍼 정의
    
    * chore: 단어 리스트 조회 함수 작성
    
    * feat: 단어 리스트 조회 api 연동
    
    * chore: 예외 처리
    
    * chore: created_at 삭제
    
    * chore: index.ts로 api 함수 정의 위치 변경
    
    * chore: 컨벤션 따른 에러 처리
    
    * chore: wordbookId non-null 단언 제거
    
    * chore: 비동기 상태 타입 정의
    
    * chore: words에 비동기 상태 타입 적용

--- src/components/organisms/WordList/WordList.tsx
+++ src/components/organisms/WordList/WordList.tsx
@@ -1,14 +1,15 @@
-import { WordDetail, type WordDetailProps } from '../../molecules/WordDetail/WordDetail';
+import type { Word } from '../../../domain/word';
+import { WordDetail } from '../../molecules/WordDetail/WordDetail';
 
 type Props = {
-  words: WordDetailProps[];
+  words: Word[];
 };
 
 export const WordList = ({ words }: Props) => {
   return (
     <div className="flex w-full flex-col gap-[0.25rem] bg-gray-100">
-      {words.map((word, idx) => (
-        <WordDetail key={idx} engWord={word.engWord} korWord={word.korWord} />
+      {words.map((word) => (
+        <WordDetail key={word.id} word={word} />
       ))}
     </div>
   );
