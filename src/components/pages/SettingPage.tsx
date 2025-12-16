402a0150c2126799a384b24811ee9c58658235f0
Emithen <86219540+Emithen@users.noreply.github.com>
2025-12-16 15:11:17 +0900

    feat: setting 페이지 data fetch 비동기 상태 관리

--- src/components/pages/SettingPage.tsx
+++ src/components/pages/SettingPage.tsx
@@ -5,9 +5,10 @@ import { SettingList } from '../organisms/SettingList/SettingList';
 import { UserProfileCard } from '../organisms/UserProfileCard/UserProfileCard';
 import { getUserProfileData } from '../../apis/user';
 import type { Profile } from '../../domain/user';
+import type { AsyncState } from '../../shared/types/asyncState';
 
 const SettingPage = () => {
-  const [profile, setProfile] = useState<Profile | null>(null);
+  const [profile, setProfile] = useState<AsyncState<Profile>>({ status: 'idle' });
 
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
@@ -21,27 +22,34 @@ const SettingPage = () => {
   useEffect(() => {
     const fetchProfileData = async () => {
       try {
+        setProfile({ status: 'loading' });
         const data = await getUserProfileData();
-        setProfile(data);
+        setProfile({ status: 'success', data: data });
       } catch (_) {
         // TODO : 에러 발생 시 UI/UX 기획 필요
+        setProfile({ status: 'error' });
         alert('사용자 프로필 정보를 불러오는 데에 실패했습니다. 다시 시도해주세요.');
       }
     };
     fetchProfileData();
   }, []);
 
-  // TODO : 로딩 스피너 같은 거 필요할 듯
-  if (!profile) return null;
+  switch (profile.status) {
+    case 'idle':
+    case 'loading':
+      return <div>Loading...</div>;
+    case 'error':
+      return <div>Error occurred while fetching words.</div>;
+  }
 
   return (
     <div className="flex h-full w-full flex-col items-center bg-gray-100">
       <Header title="Setting Page" />
       <div className="h-[0.75rem] w-full"></div>
       <UserProfileCard
-        profileImage={profile.profile_image_url}
-        email={profile.email}
-        name={profile.name}
+        profileImage={profile.data.profile_image_url}
+        email={profile.data.email}
+        name={profile.data.name}
       />
       <div className="h-[0.5rem] w-full"></div>
       <SettingList
