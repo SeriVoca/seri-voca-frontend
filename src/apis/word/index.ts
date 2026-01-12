1c2df8b10edb2c83a1bae5961633a1487bee63b8
jinj00oo <157488644+jinj00oo@users.noreply.github.com>
2026-01-12 17:44:43 +0900

    chore: 엔드포인트 수정 (#58)

--- src/apis/word/index.ts
+++ src/apis/word/index.ts
@@ -5,7 +5,7 @@ import { mapWordListResponseToDomain } from '@/mapper/word';
 
 export const getWordList = async (wordbookId: string): Promise<Word[]> => {
   try {
-    const res = await api.get<WordResponse[]>(`/wordbook/${wordbookId}`);
+    const res = await api.get<WordResponse[]>(`/wordbooks/${wordbookId}`);
     return mapWordListResponseToDomain(res.data);
   } catch (error) {
     console.error('[ERROR] 단어 목록 조회 실패', error);
--- src/apis/wordbook/index.ts
+++ src/apis/wordbook/index.ts
@@ -4,7 +4,7 @@ import { api } from '@/apis/axiosInstance';
 
 export const getWordbookList = async (): Promise<Wordbook[]> => {
   try {
-    const res = await api.get('/wordbooks/default');
+    const res = await api.get('/curriculums/default');
     return mapWordbookList(res.data);
   } catch (error) {
     console.error('[ERROR] 단어장 목록 조회 실패: ', error);
