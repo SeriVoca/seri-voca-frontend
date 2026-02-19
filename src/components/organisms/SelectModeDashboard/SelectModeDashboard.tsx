68fc38ee6f44133e37262f62cdea5449daeece36
Emithen <86219540+Emithen@users.noreply.github.com>
2026-02-19 15:18:02 +0900

    fea: select mode dash board layout 설계

--- src/components/organisms/SelectModeDashboard/SelectModeDashboard.tsx
+++ src/components/organisms/SelectModeDashboard/SelectModeDashboard.tsx
@@ -1,9 +1,50 @@
+const mock = {
+  title: '테스트 단어장',
+  selectedNum: 1,
+  totalNum: 30,
+};
+
 export const SelectModeDashboard = () => {
   return (
-    <div className="fixed bottom-0 left-1/2 z-10 w-full -translate-x-1/2 sm:max-w-[360px]">
-      {/* 진행 상황 섹션 */}
+    <div className="fixed bottom-0 left-1/2 z-10 w-full -translate-x-1/2 shadow-[0_-8px_30px_rgb(0,0,0,0.2)] sm:max-w-[360px]">
+      <div className="flex w-full flex-col items-center justify-between gap-4 px-5 py-4">
+        {/* 진행 상황 섹션 */}
+        <ProgressSection
+          title={mock.title}
+          selectedNum={mock.selectedNum}
+          totalNum={mock.totalNum}
+        />
 
-      {/* 액션 버튼 섹션 */}
+        {/* 액션 버튼 섹션 */}
+        <ActionSection />
+      </div>
+    </div>
+  );
+};
+
+type ProgressSectionProps = {
+  title: string;
+  selectedNum: number;
+  totalNum: number;
+};
+
+const ProgressSection = ({ title, selectedNum, totalNum }: ProgressSectionProps) => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div>{title}</div>
+      <div>
+        {selectedNum}/{totalNum}
+      </div>
+    </div>
+  );
+};
+
+const ActionSection = () => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <button>전체 선택</button>
+      <button>확인</button>
+      <button>취소</button>
     </div>
   );
 };
