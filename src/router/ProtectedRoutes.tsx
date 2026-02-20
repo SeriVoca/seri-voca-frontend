fb760c556b7009b1faa312d19f4f09c93bb921bb
jinj00oo <icems0428@naver.com>
2026-02-20 18:05:13 +0900

    chore: 파일명 오타 삭제

--- src/router/ProtectedRoutes.tsx
+++ src/router/ProtectedRoutes.tsx
@@ -8,7 +8,7 @@ import { HomePage } from '@/components/pages/HomePage/HomePage';
 import { WordbooksPage } from '@/components/pages/WordbooksPage/WordbooksPage';
 import { SettingPage } from '@/components/pages/SettingPage/SettingPage';
 import { WordbookDetailPage } from '@/components/pages/WordbookDetailPage/WordbookDetailPage';
-import { UserWordbookDetailPage } from '@/components/pages/UserWordbookDetailPage.tsx/UserWordbookDetailPage';
+import { UserWordbookDetailPage } from '@/components/pages/UserWordbookDetailPage/UserWordbookDetailPage';
 
 export const protectedRoutes: RouteObject[] = [
   {
