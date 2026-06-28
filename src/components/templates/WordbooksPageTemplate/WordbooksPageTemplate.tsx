00bed9438ea9b116abb831efac3474199171d6ee
jinj00oo <icems0428@naver.com>
2026-06-28 17:03:09 +0900

    Revert "chore: 시스템 단어장 목록에서 단어장 추가 버튼 삭제"
    This reverts commit 66a726791724e585819884351f0b0b7ff9258cb0.

--- src/components/templates/WordbooksPageTemplate/WordbooksPageTemplate.tsx
+++ src/components/templates/WordbooksPageTemplate/WordbooksPageTemplate.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/organisms/Header/Header';
+import { ModalPortal } from '@/components/organisms/ModalPortal/ModalPortal';
 import { TabSwitcher, type Tab } from '@/components/organisms/TabSwitcher/TabSwitcher';
 import { WordbookList } from '@/components/organisms/WordbookList/WordbookList';
 import type { Wordbook } from '@/domain/wordbook';
@@ -9,6 +10,8 @@ type Props = {
   handleTabClick: (tab: Tab) => Promise<void> | void;
   wordbooks: Wordbook[];
   handleNavigate: (id: string) => Promise<void> | void;
+  openWordbookCreateModal: (modalId: string) => void;
+  createWordbookModalContent: React.ReactNode;
 };
 
 export const WordbooksPageTemplate = ({
@@ -17,16 +20,25 @@ export const WordbooksPageTemplate = ({
   handleTabClick,
   wordbooks,
   handleNavigate,
+  openWordbookCreateModal,
+  createWordbookModalContent,
 }: Props) => {
   return (
     <div className="flex h-full w-full flex-col items-center bg-gray-100">
       <Header title="Wordbooks Page" variant="basic" />
       <main className="mt-[2.25rem] flex min-h-0 w-full flex-1 flex-col items-center px-[1.25rem]">
         <TabSwitcher tabs={tabs} activeTab={activeTab} onChange={handleTabClick} />
-        <div className="mt-[1.25rem] mb-[2.25rem] flex w-full flex-1 flex-col overflow-y-auto">
+        <div className="mt-[1.25rem] mb-[2.25rem] flex w-full flex-1 flex-col gap-[1rem] overflow-y-auto">
           <WordbookList wordbooks={wordbooks} handleNavigate={handleNavigate} />
+          <button
+            className="flex h-[36px] items-center justify-center rounded-full bg-[#e9e9e9] py-[8px] text-lg text-[#9d9d9d]"
+            onClick={() => openWordbookCreateModal('wordbook-create-modal')}
+          >
+            <div className="flex items-center justify-center leading-none">+</div>
+          </button>
         </div>
       </main>
+      <ModalPortal id="wordbook-create-modal">{createWordbookModalContent}</ModalPortal>
     </div>
   );
 };
