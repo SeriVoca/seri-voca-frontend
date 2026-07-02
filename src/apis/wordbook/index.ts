a7976cb13a92de66e5dbcdcdcd023dc01420dbf6
Jisang Kim <86219540+Emithen@users.noreply.github.com>
2026-07-02 17:23:33 +0900

    [FEAT] 사용자 단어장 생성 api 연동 (#82)
    * refactor: order index 포함 여부에 따라 mapper 이름 변경
    
    * feat: 사용자 단어장 목록 조회 api 호출 함수를 useCallback 으로 선언한 뒤 단어장 생성 후 재조회

--- src/apis/wordbook/index.ts
+++ src/apis/wordbook/index.ts
@@ -1,11 +1,11 @@
 import type { Wordbook } from '@/domain/wordbook';
-import { mapWordbookList, mapUserWordbookList } from '@/mapper/wordbook';
+import { mapOrderedWordbookList, mapWordbook, mapWordbookList } from '@/mapper/wordbook';
 import { api } from '@/apis/axiosInstance';
 
 export const getWordbookList = async (): Promise<Wordbook[]> => {
   try {
     const res = await api.get('/curriculums/default');
-    return mapWordbookList(res.data);
+    return mapOrderedWordbookList(res.data);
   } catch (error) {
     console.error('[ERROR] 단어장 목록 조회 실패: ', error);
     throw error;
@@ -15,9 +15,22 @@ export const getWordbookList = async (): Promise<Wordbook[]> => {
 export const getUserWordbookList = async (): Promise<Wordbook[]> => {
   try {
     const res = await api.get('/wordbooks/user');
-    return mapUserWordbookList(res.data);
+    return mapWordbookList(res.data);
   } catch (error) {
     console.error('[ERROR] 사용자 단어장 목록 조회 실패: ', error);
+    throw error;
+  }
+};
+
+export const createUserWordbook = async (
+  title: string,
+  description: string | null,
+): Promise<Wordbook> => {
+  try {
+    const res = await api.post('/wordbooks', { title, description });
+    return mapWordbook(res.data);
+  } catch (error) {
+    console.error('[ERROR] 사용자 단어장 생성 실패: ', error);
     throw error;
   }
 };
