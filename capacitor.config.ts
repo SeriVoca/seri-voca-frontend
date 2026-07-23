d5c946a408b7dfde34aaa2c457fd53917f0a4209
Jisang Kim <86219540+Emithen@users.noreply.github.com>
2026-07-24 00:57:05 +0900

    chore: 실행 성공

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.serivoca.app',
+  appName: 'SeriVoca',
+  webDir: 'dist',
+};
+
+export default config;
